gaul/s3proxy

commit e5c12c177756a6aeb28f4ad840c68dfa2e25690b

more required parameters needed for signing
allows s3cmd info s3://bucket/ to work

src/main/java/org/gaul/s3proxy/S3ProxyHandler.java
@@ -44,6 +44,7 @@ import com.google.common.base.Preconditions;
 import com.google.common.base.Strings;
 import com.google.common.base.Throwables;
 import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.ImmutableSet;
 import com.google.common.collect.SortedSetMultimap;
 import com.google.common.collect.TreeMultimap;
 import com.google.common.hash.HashCode;
@@ -97,6 +98,11 @@ final class S3ProxyHandler extends AbstractHandler {
             Pattern.compile("<LocationConstraint>(.*?)</LocationConstraint>");
     private static final Pattern MULTI_DELETE_KEY_PATTERN =
             Pattern.compile("<Key>(.*?)</Key>");
+    private static final Set<String> SIGNED_SUBRESOURCES = ImmutableSet.of(
+            "acl", "lifecycle", "location", "logging", "notification",
+            "partNumber", "policy", "requestPayment", "torrent", "uploadId",
+            "uploads", "versionId", "versioning", "versions", "website"
+    );
 
     private final BlobStore blobStore;
     private final String identity;
@@ -997,11 +1003,21 @@ final class S3ProxyHandler extends AbstractHandler {
                     .append(entry.getValue()).append('\n');
         }
         builder.append(uri);
-        if ("".equals(request.getParameter("acl"))) {
-            builder.append("?acl");
-        } else if ("".equals(request.getParameter("delete"))) {
-            builder.append("?delete");
+
+        char separator = '?';
+        for (String subresource : Collections.list(
+                request.getParameterNames())) {
+            if (SIGNED_SUBRESOURCES.contains(subresource)) {
+                builder.append(separator).append(subresource);
+
+                String value = request.getParameter(subresource);
+                if (!"".equals(value)) {
+                    builder.append('=').append(value);
+                }
+                separator = '&';
+            }
         }
+
         String stringToSign = builder.toString();
         logger.trace("stringToSign: {}", stringToSign);
 

commit 161fd3d8476d1a61b25124168d4d4d8244be5510

Include delete as a signed resource
This allows multi-object delete to work and addresses a regression
introduced by e5c12c177756a6aeb28f4ad840c68dfa2e25690b.

src/main/java/org/gaul/s3proxy/S3ProxyHandler.java
@@ -100,7 +100,7 @@ final class S3ProxyHandler extends AbstractHandler {
     private static final Pattern MULTI_DELETE_KEY_PATTERN =
             Pattern.compile("<Key>(.*?)</Key>");
     private static final Set<String> SIGNED_SUBRESOURCES = ImmutableSet.of(
-            "acl", "lifecycle", "location", "logging", "notification",
+            "acl", "delete", "lifecycle", "location", "logging", "notification",
             "partNumber", "policy", "requestPayment", "torrent", "uploadId",
             "uploads", "versionId", "versioning", "versions", "website"
     );