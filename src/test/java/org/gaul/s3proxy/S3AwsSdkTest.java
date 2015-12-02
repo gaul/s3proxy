a5221f5f8e51c639c958dfca5fa9799b572d1803
Rita Zhang <rita.z.zhang@gmail.com>
2015-12-01 16:30:20 -0800

    Upload two parts in testBigMultipartUpload

--- src/test/java/org/gaul/s3proxy/S3AwsSdkTest.java
+++ src/test/java/org/gaul/s3proxy/S3AwsSdkTest.java
@@ -220,6 +220,7 @@ public final class S3AwsSdkTest {
 
         String key = "multipart-upload";
         int size = 10_000_000;
+        int partSize = 5 * 1024 * 1024;
         ByteSource byteSource = TestUtils.randomByteSource().slice(0, size);
 
         InitiateMultipartUploadRequest initRequest =
@@ -232,13 +233,25 @@ public final class S3AwsSdkTest {
                 .withBucketName(containerName).withKey(key)
                 .withUploadId(uploadId).withPartNumber(1)
                 .withInputStream(byteSource.openStream())
-                .withPartSize(size);
+                .withPartSize(partSize);
 
         UploadPartResult uploadPartResult = client.uploadPart(uploadRequest);
         PartETag partETag = uploadPartResult.getPartETag();
+
+        UploadPartRequest uploadRequest2 = new UploadPartRequest()
+                .withBucketName(containerName).withKey(key)
+                .withUploadId(uploadId).withPartNumber(2)
+                .withInputStream(byteSource
+                        .slice(partSize, size - partSize).openStream())
+                .withPartSize(size - partSize);
+
+        UploadPartResult uploadPartResult2 = client.uploadPart(uploadRequest2);
+        PartETag partETag2 = uploadPartResult2.getPartETag();
+
         // must be mutable since AWK SDK sorts parts
         List<PartETag> partETagList = new ArrayList<PartETag>();
         partETagList.add(partETag);
+        partETagList.add(partETag2);
 
         CompleteMultipartUploadRequest completeRequest = new
                 CompleteMultipartUploadRequest(
