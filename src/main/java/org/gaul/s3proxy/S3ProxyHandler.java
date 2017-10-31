gaul/s3proxy

commit fb0f321303596d27ac9e9ef4ef216b9562746660

Improve CommonPrefixes support
References #5.

src/main/java/org/gaul/s3proxy/S3ProxyHandler.java
@@ -73,7 +73,6 @@ import org.jclouds.blobstore.domain.BlobBuilder;
 import org.jclouds.blobstore.domain.BlobMetadata;
 import org.jclouds.blobstore.domain.PageSet;
 import org.jclouds.blobstore.domain.StorageMetadata;
-import org.jclouds.blobstore.domain.StorageType;
 import org.jclouds.blobstore.options.CreateContainerOptions;
 import org.jclouds.blobstore.options.GetOptions;
 import org.jclouds.blobstore.options.ListContainerOptions;
@@ -700,10 +699,25 @@ final class S3ProxyHandler extends AbstractHandler {
 
             Set<String> commonPrefixes = new TreeSet<>();
             for (StorageMetadata metadata : set) {
-                if (metadata.getType() != StorageType.BLOB) {
-                    commonPrefixes.add(metadata.getName());
+                switch (metadata.getType()) {
+                case FOLDER:
                     continue;
+                case RELATIVE_PATH:
+                    String name = metadata.getName();
+                    if (delimiter != null) {
+                        int index = name.indexOf(delimiter,
+                                Strings.nullToEmpty(prefix).length());
+                        if (index != -1) {
+                            name = name.substring(0, index + 1);
+                        }
+                        name += delimiter;
+                    }
+                    commonPrefixes.add(name);
+                    continue;
+                default:
+                    break;
                 }
+
                 xml.writeStartElement("Contents");
 
                 xml.writeStartElement("Key");
@@ -759,13 +773,7 @@ final class S3ProxyHandler extends AbstractHandler {
                 xml.writeStartElement("CommonPrefixes");
 
                 xml.writeStartElement("Prefix");
-                if (prefix != null) {
-                    xml.writeCharacters(prefix);
-                }
                 xml.writeCharacters(commonPrefix);
-                if (delimiter != null) {
-                    xml.writeCharacters(delimiter);
-                }
                 xml.writeEndElement();
 
                 xml.writeEndElement();

commit a09c9e1998599dbc573b08c5e82a2988fac8291f

Treat directories as relative paths
When listing blobs foo/1 and foo/2 with prefix foo and delimiter /,
jclouds returns foo as a relative path, except for Atmos which returns
foo as a directory.  Treat the latter like the former to allow listing
pseudo-directories with Atmos.
fb0f321303596d27ac9e9ef4ef216b9562746660 introduced this regression.
Fixes #244.

src/main/java/org/gaul/s3proxy/S3ProxyHandler.java
@@ -1291,7 +1291,7 @@ public class S3ProxyHandler {
             for (StorageMetadata metadata : set) {
                 switch (metadata.getType()) {
                 case FOLDER:
-                    continue;
+                    // fallthrough
                 case RELATIVE_PATH:
                     commonPrefixes.add(metadata.getName());
                     continue;