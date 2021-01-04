73b8f63df9a3843e77dd3b621e6eae082a3b1c9e
johnnyaug <yoni.augarten@treeverse.io>
2021-01-04 16:47:48 +0200

    add virtual host to docker

--- src/main/resources/run-docker-container.sh
+++ src/main/resources/run-docker-container.sh
@@ -7,6 +7,7 @@ exec java \
     -Ds3proxy.authorization="${S3PROXY_AUTHORIZATION}" \
     -Ds3proxy.identity="${S3PROXY_IDENTITY}" \
     -Ds3proxy.credential="${S3PROXY_CREDENTIAL}" \
+    -Ds3proxy.virtual-host="${S3PROXY_VIRTUAL_HOST}" \
     -Ds3proxy.cors-allow-all="${S3PROXY_CORS_ALLOW_ALL}" \
     -Ds3proxy.cors-allow-origins="${S3PROXY_CORS_ALLOW_ORIGINS}" \
     -Ds3proxy.cors-allow-methods="${S3PROXY_CORS_ALLOW_METHODS}" \
