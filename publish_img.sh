5da4dfc9e42e4c561546354625cbfea73f3f31bf
Nicklas Ansman Giertz <nicklas@ansman.se>
2021-11-15 11:10:08 -0500

    Fix a missing variable when publishing

--- publish_img.sh
+++ publish_img.sh
@@ -4,9 +4,9 @@ docker login -u "$DOCKER_USER" -p "$DOCKER_PASS"
 docker buildx build --platform linux/amd64 -t $GITHUB_REPOSITORY:latest .
 
 if [[ "$GITHUB_EVENT_NAME" == "push" && $GITHUB_REF == refs/heads/master ]]; then
-  docker tag $REPO:latest $GITHUB_REPOSITORY:${GITHUB_SHA::8}
+  docker tag $GITHUB_REPOSITORY:latest $GITHUB_REPOSITORY:${GITHUB_SHA::8}
   docker push --all-tags $GITHUB_REPOSITORY
 elif [[ "$GITHUB_EVENT_NAME" == "create" && $GITHUB_REF == refs/tags/* ]]; then
-  docker tag $REPO:latest $GITHUB_REPOSITORY:${GITHUB_REF#refs/tags/}
+  docker tag $GITHUB_REPOSITORY:latest $GITHUB_REPOSITORY:${GITHUB_REF#refs/tags/}
   docker push --all-tags $GITHUB_REPOSITORY
 fi
