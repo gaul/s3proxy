28c29bb1163aac8ef7c42eafbf4b8b3bcfe040ef
Andrew Gaul <andrew@gaul.org>
2021-11-16 09:13:01 +0900

    Refer to Docker Hub andrewgaul/s3proxy repo
    This differs in organization from the GitHub gaul/s3proxy repo.
    References #388.

--- publish_img.sh
+++ publish_img.sh
@@ -4,13 +4,15 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
+REPO=andrewgaul/s3proxy
+
 docker login -u "$DOCKER_USER" -p "$DOCKER_PASS"
-docker buildx build --platform linux/amd64 -t $GITHUB_REPOSITORY:latest .
+docker buildx build --platform linux/amd64 -t $REPO:latest .
 
 if [[ "$GITHUB_EVENT_NAME" == "push" && $GITHUB_REF == refs/heads/master ]]; then
-  docker tag $GITHUB_REPOSITORY:latest $GITHUB_REPOSITORY:${GITHUB_SHA::8}
-  docker push --all-tags $GITHUB_REPOSITORY
+  docker tag $REPO:latest $REPO:${GITHUB_SHA::8}
+  docker push --all-tags $REPO
 elif [[ "$GITHUB_EVENT_NAME" == "create" && $GITHUB_REF == refs/tags/* ]]; then
-  docker tag $GITHUB_REPOSITORY:latest $GITHUB_REPOSITORY:${GITHUB_REF#refs/tags/}
-  docker push --all-tags $GITHUB_REPOSITORY
+  docker tag $REPO:latest $REPO:${GITHUB_REF#refs/tags/}
+  docker push --all-tags $REPO
 fi
