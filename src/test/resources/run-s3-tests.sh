812a4d38a756edd0f9c71f7abccc27a3a548c972
Andrew Gaul <andrew@gaul.org>
2024-09-06 10:46:43 +0200

    Rebase s3-tests

--- src/test/resources/run-s3-tests.sh
+++ src/test/resources/run-s3-tests.sh
@@ -7,11 +7,6 @@ S3PROXY_BIN="${PWD}/target/s3proxy"
 S3PROXY_PORT="${S3PROXY_PORT:-8081}"
 export S3TEST_CONF="${PWD}/src/test/resources/s3-tests.conf"
 
-# configure s3-tests
-pushd s3-tests
-./bootstrap
-popd
-
 # launch S3Proxy using HTTP and a fixed port
 sed "s,^\(s3proxy.endpoint\)=.*,\1=http://127.0.0.1:${S3PROXY_PORT}," \
         < src/test/resources/s3proxy.conf | grep -v secure-endpoint > target/s3proxy.conf
@@ -37,4 +32,4 @@ done
 
 # execute s3-tests
 pushd s3-tests
-./virtualenv/bin/nosetests -a '!fails_on_s3proxy,!appendobject,!bucket-policy,!cors,!encryption,!fails_strict_rfc2616,!lifecycle,!object-lock,!policy,!policy_status,!s3select,!s3website,!tagging,!user-policy,!versioning'
+tox -- -m 'not fails_on_s3proxy and not appendobject and not bucket_policy and not checksum and not cors and not encryption and not fails_strict_rfc2616 and not iam_tenant and not lifecycle and not object_lock and not policy and not policy_status and not s3select and not s3website and not sse_s3 and not tagging and not test_of_sts and not user_policy and not versioning and not webidentity_test'
