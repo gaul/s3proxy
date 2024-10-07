c3c9037d195af5b6900faf310732cc99b21285c6
Andrew Gaul <andrew@gaul.org>
2024-10-06 18:17:23 -0700

    Close InputStream
    Found via Coverity.

--- src/main/java/org/gaul/s3proxy/crypto/PartPadding.java
+++ src/main/java/org/gaul/s3proxy/crypto/PartPadding.java
@@ -43,31 +43,31 @@ public class PartPadding {
             throws IOException {
         PartPadding partPadding = new PartPadding();
 
-        InputStream is = blob.getPayload().openStream();
+        try (var is = blob.getPayload().openStream()) {
+            byte[] paddingBytes = IOUtils.toByteArray(is);
+            ByteBuffer bb = ByteBuffer.wrap(paddingBytes);
 
-        byte[] paddingBytes = IOUtils.toByteArray(is);
-        ByteBuffer bb = ByteBuffer.wrap(paddingBytes);
+            byte[] delimiterBytes = new byte[Constants.PADDING_DELIMITER_LENGTH];
+            bb.get(delimiterBytes);
+            partPadding.delimiter =
+                new String(delimiterBytes, StandardCharsets.UTF_8);
 
-        byte[] delimiterBytes = new byte[Constants.PADDING_DELIMITER_LENGTH];
-        bb.get(delimiterBytes);
-        partPadding.delimiter =
-            new String(delimiterBytes, StandardCharsets.UTF_8);
+            byte[] ivBytes = new byte[Constants.PADDING_IV_LENGTH];
+            bb.get(ivBytes);
+            partPadding.iv = new IvParameterSpec(ivBytes);
 
-        byte[] ivBytes = new byte[Constants.PADDING_IV_LENGTH];
-        bb.get(ivBytes);
-        partPadding.iv = new IvParameterSpec(ivBytes);
+            partPadding.part = bb.getInt();
+            partPadding.size = bb.getLong();
+            partPadding.version = bb.getShort();
 
-        partPadding.part = bb.getInt();
-        partPadding.size = bb.getLong();
-        partPadding.version = bb.getShort();
+            logger.debug("delimiter {}", partPadding.delimiter);
+            logger.debug("iv {}", Arrays.toString(ivBytes));
+            logger.debug("part {}", partPadding.part);
+            logger.debug("size {}", partPadding.size);
+            logger.debug("version {}", partPadding.version);
 
-        logger.debug("delimiter {}", partPadding.delimiter);
-        logger.debug("iv {}", Arrays.toString(ivBytes));
-        logger.debug("part {}", partPadding.part);
-        logger.debug("size {}", partPadding.size);
-        logger.debug("version {}", partPadding.version);
-
-        return partPadding;
+            return partPadding;
+        }
     }
 
     public final String getDelimiter() {
