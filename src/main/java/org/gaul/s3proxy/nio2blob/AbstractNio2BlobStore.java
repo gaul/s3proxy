gaul/s3proxy

commit fef17b0cf8b5d9242f574e228e5f0a813fd45406

Eagerly check baseDir in FilesystemNio2BlobStore

src/main/java/org/gaul/s3proxy/nio2blob/AbstractNio2BlobStore.java
@@ -128,6 +128,10 @@ public abstract class AbstractNio2BlobStore extends BaseBlobStore {
         this.root = root;
     }
 
+    protected Path getRoot() {
+        return root;
+    }
+
     @Override
     public final Set<? extends Location> listAssignableLocations() {
         return locations.get();

src/main/java/org/gaul/s3proxy/nio2blob/FilesystemNio2BlobStore.java
@@ -17,6 +17,8 @@
 package org.gaul.s3proxy.nio2blob;
 
 import java.nio.file.FileSystems;
+import java.nio.file.Files;
+import java.nio.file.NoSuchFileException;
 import java.util.Set;
 
 import com.google.common.base.Supplier;
@@ -45,5 +47,8 @@ public final class FilesystemNio2BlobStore extends AbstractNio2BlobStore {
         super(context, blobUtils, defaultLocation, locations, slicer, creds,
                 // cannot be closed
                 FileSystems.getDefault().getPath(baseDir));
+        if (!Files.exists(getRoot())) {
+            throw new RuntimeException(new NoSuchFileException(getRoot().toString()));
+        }
     }
 }

commit 53976f6e48b6a1d78369fa074a9c9f3985eb976a

Fix Checkstyle violation
Regression introduced in fef17b0cf8b5d9242f574e228e5f0a813fd45406.

src/main/java/org/gaul/s3proxy/nio2blob/AbstractNio2BlobStore.java
@@ -128,7 +128,7 @@ public abstract class AbstractNio2BlobStore extends BaseBlobStore {
         this.root = root;
     }
 
-    protected Path getRoot() {
+    protected final Path getRoot() {
         return root;
     }
 