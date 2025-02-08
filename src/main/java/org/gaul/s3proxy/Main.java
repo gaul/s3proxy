gaul/s3proxy

commit 4976e170c3c068275201a79d329f953f685fb808

Replace uses of Maps.immutableEntry with Map.entry
The latter does not support null keys or values so this cannot replace
all uses.

src/main/java/org/gaul/s3proxy/Main.java
@@ -41,7 +41,6 @@ import com.google.common.base.Strings;
 import com.google.common.collect.ImmutableBiMap;
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableMap;
-import com.google.common.collect.Maps;
 import com.google.common.io.Files;
 import com.google.common.util.concurrent.ThreadFactoryBuilder;
 import com.google.inject.Module;
@@ -137,7 +136,7 @@ public final class Main {
                         S3ProxyConstants.PROPERTY_CREDENTIAL);
                 if (parsedIdentities.add(localIdentity)) {
                     locators.put(localIdentity,
-                            Maps.immutableEntry(localCredential, blobStore));
+                            Map.entry(localCredential, blobStore));
                 }
             }
             for (String key : properties.stringPropertyNames()) {
@@ -147,7 +146,7 @@ public final class Main {
                         globLocators.put(
                                 FileSystems.getDefault().getPathMatcher(
                                         "glob:" + bucketLocator),
-                                Maps.immutableEntry(localIdentity, blobStore));
+                                Map.entry(localIdentity, blobStore));
                     } else {
                         System.err.println("Multiple definitions of the " +
                                 "bucket locator: " + bucketLocator);

src/main/java/org/gaul/s3proxy/GlobBlobStoreLocator.java
@@ -70,7 +70,6 @@ public final class GlobBlobStoreLocator implements BlobStoreLocator {
         if (locatorEntry == null) {
             return null;
         }
-        return Maps.immutableEntry(locatorEntry.getKey(),
-                globEntry.getValue());
+        return Map.entry(locatorEntry.getKey(), globEntry.getValue());
     }
 }

src/main/java/org/gaul/s3proxy/S3ProxyHandler.java
@@ -241,7 +241,7 @@ public class S3ProxyHandler {
                     if (!identity.equals(identityArg)) {
                         return null;
                     }
-                    return Maps.immutableEntry(credential, blobStore);
+                    return Map.entry(credential, blobStore);
                 }
             };
         } else {
@@ -1400,7 +1400,7 @@ public class S3ProxyHandler {
         if (marker != null) {
             if (Quirks.OPAQUE_MARKERS.contains(blobStoreType)) {
                 String realMarker = lastKeyToMarker.getIfPresent(
-                        Maps.immutableEntry(containerName, marker));
+                        Map.entry(containerName, marker));
                 if (realMarker != null) {
                     marker = realMarker;
                 }
@@ -1494,7 +1494,7 @@ public class S3ProxyHandler {
                     StorageMetadata sm = Streams.findLast(
                             set.stream()).orElse(null);
                     if (sm != null) {
-                        lastKeyToMarker.put(Maps.immutableEntry(
+                        lastKeyToMarker.put(Map.entry(
                                 containerName,
                                 encodeBlob(encodingType, nextMarker)),
                                 nextMarker);

src/test/java/org/gaul/s3proxy/AwsSdkTest.java
@@ -92,7 +92,6 @@ import com.amazonaws.services.s3.model.UploadPartRequest;
 import com.amazonaws.services.s3.model.UploadPartResult;
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableMap;
-import com.google.common.collect.Maps;
 import com.google.common.io.ByteSource;
 
 import org.assertj.core.api.Fail;
@@ -1518,10 +1517,9 @@ public final class AwsSdkTest {
             public Map.Entry<String, BlobStore> locateBlobStore(
                     String identity, String container, String blob) {
                 if (identity.equals(awsCreds.getAWSAccessKeyId())) {
-                    return Maps.immutableEntry(awsCreds.getAWSSecretKey(),
-                            blobStore1);
+                    return Map.entry(awsCreds.getAWSSecretKey(), blobStore1);
                 } else if (identity.equals("other-identity")) {
-                    return Maps.immutableEntry("credential", blobStore2);
+                    return Map.entry("credential", blobStore2);
                 } else {
                     return null;
                 }

src/test/java/org/gaul/s3proxy/GlobBlobStoreLocatorTest.java
@@ -57,8 +57,8 @@ public final class GlobBlobStoreLocatorTest {
     @Test
     public void testLocateIdentity() {
         var credsMap = ImmutableSortedMap.of(
-                "id1", Maps.immutableEntry("one", blobStoreOne),
-                "id2", Maps.immutableEntry("two", blobStoreTwo));
+                "id1", Map.entry("one", blobStoreOne),
+                "id2", Map.entry("two", blobStoreTwo));
         GlobBlobStoreLocator locator = new GlobBlobStoreLocator(
                 credsMap, ImmutableMap.of());
         assertThat(locator.locateBlobStore("id2", null, null).getKey())
@@ -71,13 +71,13 @@ public final class GlobBlobStoreLocatorTest {
     @Test
     public void testLocateContainer() {
         var credsMap = ImmutableMap.of(
-                "id1", Maps.immutableEntry("one", blobStoreOne),
-                "id2", Maps.immutableEntry("two", blobStoreTwo));
+                "id1", Map.entry("one", blobStoreOne),
+                "id2", Map.entry("two", blobStoreTwo));
         var globMap = ImmutableMap.of(
                 FileSystems.getDefault().getPathMatcher("glob:container1"),
-                Maps.immutableEntry("id1", blobStoreOne),
+                Map.entry("id1", blobStoreOne),
                 FileSystems.getDefault().getPathMatcher("glob:container2"),
-                Maps.immutableEntry("id2", blobStoreTwo));
+                Map.entry("id2", blobStoreTwo));
         GlobBlobStoreLocator locator = new GlobBlobStoreLocator(credsMap,
                 globMap);
 
@@ -100,15 +100,15 @@ public final class GlobBlobStoreLocatorTest {
         var credsMap =
                 ImmutableSortedMap.<String, Map.Entry<String, BlobStore>>of(
                     "id0", Maps.immutableEntry("zero", null),
-                    "id1", Maps.immutableEntry("one", blobStoreOne),
-                    "id2", Maps.immutableEntry("two", blobStoreTwo));
+                    "id1", Map.entry("one", blobStoreOne),
+                    "id2", Map.entry("two", blobStoreTwo));
         var globMap =
                 ImmutableMap.<PathMatcher, Map.Entry<String, BlobStore>>of(
                         FileSystems.getDefault().getPathMatcher(
                                 "glob:{one,two}"),
-                        Maps.immutableEntry("id1", blobStoreOne),
+                        Map.entry("id1", blobStoreOne),
                         FileSystems.getDefault().getPathMatcher("glob:cont?X*"),
-                        Maps.immutableEntry("id2", blobStoreTwo));
+                        Map.entry("id2", blobStoreTwo));
         GlobBlobStoreLocator locator = new GlobBlobStoreLocator(credsMap,
                 globMap);
 

commit b6ce601c3b4cef886e8c95aa84b02f00120c3609

Allow null localIdentify with bucket locators
This can be null when using none authorization.  Regression from
4976e170c3c068275201a79d329f953f685fb808.  Fixes #781.

src/main/java/org/gaul/s3proxy/Main.java
@@ -43,6 +43,7 @@ import com.google.common.base.Strings;
 import com.google.common.base.Supplier;
 import com.google.common.collect.ImmutableBiMap;
 import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.Maps;
 import com.google.common.io.MoreFiles;
 import com.google.common.util.concurrent.ThreadFactoryBuilder;
 
@@ -154,7 +155,7 @@ public final class Main {
                         globLocators.put(
                                 FileSystems.getDefault().getPathMatcher(
                                         "glob:" + bucketLocator),
-                                Map.entry(localIdentity, blobStore));
+                                Maps.immutableEntry(localIdentity, blobStore));
                     } else {
                         System.err.println("Multiple definitions of the " +
                                 "bucket locator: " + bucketLocator);