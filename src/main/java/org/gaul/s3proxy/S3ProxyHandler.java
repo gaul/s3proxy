810e13875688f663fcf5251c716326326cc0da88
Nico Duldhardt <nico@duldhardt.com>
2025-12-11 19:36:38 +0100

    Remove unused imports

--- src/main/java/org/gaul/s3proxy/S3ProxyHandler.java
+++ src/main/java/org/gaul/s3proxy/S3ProxyHandler.java
@@ -38,7 +38,6 @@ import java.util.Base64;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.Date;
-import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
