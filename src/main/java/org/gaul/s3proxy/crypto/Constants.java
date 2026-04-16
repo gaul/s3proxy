cc29a1f66523d84a7c6054b2cb78e84461bc762f
Andrew Gaul <andrew@gaul.org>
2026-04-16 08:27:11 -0700

    Tighten ETag regex so it matches the suffix
    Also remove unneeded .* since callers call find().

--- src/main/java/org/gaul/s3proxy/crypto/Constants.java
+++ src/main/java/org/gaul/s3proxy/crypto/Constants.java
@@ -25,7 +25,7 @@ public final class Constants {
     public static final String S3_ENC_SUFFIX = ".s3enc";
     public static final String MPU_FOLDER = ".mpu/";
     public static final Pattern MPU_ETAG_SUFFIX_PATTERN =
-        Pattern.compile(".*-([0-9]+)");
+        Pattern.compile("-([0-9]+)$");
     public static final String METADATA_ENCRYPTION_PARTS =
         "s3proxy_encryption_parts";
     public static final String METADATA_IS_ENCRYPTED_MULTIPART =
